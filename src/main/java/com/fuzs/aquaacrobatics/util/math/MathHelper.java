2d698a294fca4ce12458338ec4d8f3b2420cda49
Li <nhatlinh.l195@gmail.com>
2021-08-03 10:52:53 +0700

    Update MathHelper.java

--- src/main/java/com/fuzs/aquaacrobatics/util/math/MathHelper.java
+++ src/main/java/com/fuzs/aquaacrobatics/util/math/MathHelper.java
@@ -1,6 +1,8 @@
 package com.fuzs.aquaacrobatics.util.math;
 
 public class MathHelper extends net.minecraft.util.math.MathHelper {
+    
+    MathHelper mathhelper = new MathHelper();
 
     public static float lerp(float pct, float start, float end) {
         return start + pct * (end - start);
